9bdec5af7ec2d2ad7f34f49e23a52898ef0dfe2e
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2018-11-06 12:42:19 +0900

    excluding too large auto-generated class from code coverage

--- test/__init__.py
+++ test/__init__.py
@@ -8,7 +8,8 @@ from open_fortran_parser.config import JAVA as java_config
 
 if os.environ.get('TEST_COVERAGE'):
     JACOCO_PATH = pathlib.Path('lib', 'org.jacoco.agent-0.8.1-runtime.jar').resolve()
-    JACOCO_EXCLUDES = ('fortran.ofp.parser.java.FortranParserExtras_FortranParser08',)
+    JACOCO_EXCLUDES = ('fortran.ofp.parser.java.FortranParserExtras_FortranParser08',
+                       'fortran.ofp.parser.java.FortranParser2008_FortranParserBase')
     if JACOCO_PATH.is_file():
         if java_config['options'] is None:
             java_config['options'] = []
